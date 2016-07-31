d4351edcea8369ec3748bc7761a677f643e0a5cb
Paul Bouwer <paul.bouwer@gmail.com>
2016-07-31 10:07:39 +1000

    Small formatting and logic changes.

--- src/scriptcs/svm/commands/uninstall.go
+++ src/scriptcs/svm/commands/uninstall.go
@@ -1,10 +1,11 @@
 package commands
 
 import (
-  "fmt"
-  "errors"  
-  "scriptcs/config"
-  "scriptcs/svm/usage"
+	"errors"
+	"fmt"
+	"scriptcs/config"
+	"scriptcs/svm/usage"
+
 	"github.com/spf13/cobra"
 )
 
@@ -12,26 +13,26 @@ import (
 // svm uninstall <version>
 
 var (
-  errInvalidUninstallUsage = errors.New("Invalid 'svm uninstall' command usage.")
-  uninstallCmd  = &cobra.Command { Use: "uninstall", RunE: uninstall, SilenceErrors: true, SilenceUsage: true }
+	errInvalidUninstallUsage = errors.New("Invalid 'svm uninstall' command usage.")
+	uninstallCmd             = &cobra.Command{Use: "uninstall", RunE: uninstall, SilenceErrors: true, SilenceUsage: true}
 )
 
 func init() {
-  uninstallCmd.SetHelpFunc( func (*cobra.Command, []string) { usage.NewUninstallMessage().Print()  } )
-  svmCmd.AddCommand(uninstallCmd)
+	uninstallCmd.SetHelpFunc(func(*cobra.Command, []string) { usage.NewUninstallMessage().Print() })
+	svmCmd.AddCommand(uninstallCmd)
 }
 
 func uninstall(cmd *cobra.Command, args []string) error {
-  if (!isValidUninstallArguments(cmd, args)) {
-    return errInvalidUninstallUsage
-  }
-  
-  fmt.Printf("uninstall command executed...\n")
-  fmt.Println(args)
-  fmt.Printf("Verbose %t\n", config.Verbose)
-  return nil
+	if !isValidUninstallArguments(cmd, args) {
+		return errInvalidUninstallUsage
+	}
+
+	fmt.Printf("uninstall command executed...\n")
+	fmt.Println(args)
+	fmt.Printf("Verbose %t\n", config.Verbose)
+	return nil
 }
 
 func isValidUninstallArguments(cmd *cobra.Command, args []string) bool {
-  return len(args) == 1
+	return len(args) == 1
 }
